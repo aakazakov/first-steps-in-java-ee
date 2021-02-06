aa76a4f7b63157db6d496cadd26c94a4867b86d3
nito nise <aakazakovv@yandex.ru>
2021-02-06 16:35:35 +0300

    add order page servlet

---
+++ hello-world/src/main/java/dev/fun/hello/servlets/OrderPageServlet.java
@@ -0,0 +1,30 @@
+package dev.fun.hello.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@WebServlet(urlPatterns = "/order")
+public class OrderPageServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+	
+	private static final Logger logger = LoggerFactory.getLogger(OrderPageServlet.class);
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException {
+		try {
+			resp.getWriter().print("<h1>Order</h1>");
+		} catch (IOException e) {
+			logger.info(e.getMessage());
+		}
+	}
+	
+}
