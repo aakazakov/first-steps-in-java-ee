2e7a1b2072e114cbc4d20793260b74e3d895e5c3
nito nise <aakazakovv@yandex.ru>
2021-02-06 16:18:58 +0300

    add logger

--- hello-world/src/main/java/dev/fun/hello/servlets/MainPageServlet.java
+++ hello-world/src/main/java/dev/fun/hello/servlets/MainPageServlet.java
@@ -8,14 +8,23 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @WebServlet(urlPatterns = "/main")
 public class MainPageServlet extends HttpServlet {
 	
 	private static final long serialVersionUID = 1L;
+	
+	private static final Logger logger = LoggerFactory.getLogger(MainPageServlet.class);
 
 	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		resp.getWriter().print("<h1>Main</h1>");
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException {
+		try {
+			resp.getWriter().print("<h1>Main</h1>");
+		} catch (IOException e) {
+			logger.info(e.getMessage());
+		}
 	}	
 
 }
