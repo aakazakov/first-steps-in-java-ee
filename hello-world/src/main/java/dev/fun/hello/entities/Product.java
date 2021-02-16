0927e08c97bf4036a54ccedcb963ff6006dcff4c
nito nise <aakazakovv@yandex.ru>
2021-02-16 12:31:51 +0300

    add entity etc

---
+++ hello-world/src/main/java/dev/fun/hello/entities/Product.java
@@ -0,0 +1,47 @@
+package dev.fun.hello.entities;
+
+public class Product {
+	
+	private Long id;
+	private String title;
+	private Float price;
+	
+	public Long getId() {
+		return id;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	public Float getPrice() {
+		return price;
+	}
+	
+	public void setPrice(Float price) {
+		this.price = price;
+	}
+	
+	public Product() {
+		
+	}
+
+	public Product(String title, Float price) {
+		this.title = title;
+		this.price = price;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("%s_{id: %d, title: %s, price: %f}", this.getClass().getSimpleName(), id, title, price);
+	}
+	
+}
---
+++ hello-world/src/main/java/dev/fun/hello/repositories/ProductRepository.java
@@ -0,0 +1,43 @@
+package dev.fun.hello.repositories;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import dev.fun.hello.entities.Product;
+
+public class ProductRepository {
+
+	private static final Logger logger = LoggerFactory.getLogger(ProductRepository.class);
+	
+	private AtomicLong ID;
+	
+	private Map<Long, Product> repo;
+	
+	public ProductRepository() {
+		repo = new HashMap<>();
+		ID = new AtomicLong(0L);
+	}
+	
+	public void add(Product p) {
+		if (p.getId() == null) {
+			p.setId(ID.incrementAndGet());
+		}
+		repo.put(p.getId(), p);
+		logger.info("[ADDED] " + p);
+	}
+	
+	public Product get(Long id) {
+		return repo.get(id);
+	}
+	
+	public List<Product> getAll() {
+		return new ArrayList<>(repo.values());
+	}
+	
+}
