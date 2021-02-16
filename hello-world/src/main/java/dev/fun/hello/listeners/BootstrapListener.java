19aa4521963687838b9e1383950be86bc6e4c180
nito nise <aakazakovv@yandex.ru>
2021-02-16 15:17:22 +0300

    add listener

---
+++ hello-world/src/main/java/dev/fun/hello/listeners/BootstrapListener.java
@@ -0,0 +1,27 @@
+package dev.fun.hello.listeners;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+import dev.fun.hello.entities.Product;
+import dev.fun.hello.repositories.ProductRepository;
+
+@WebListener
+public class BootstrapListener implements ServletContextListener {
+
+	@Override
+	public void contextInitialized(ServletContextEvent sce) {
+		ProductRepository productRepository = new ProductRepository();
+		
+		productRepository.add(new Product("Product1", 100.0f));
+		productRepository.add(new Product("Product2", 200.0f));
+		productRepository.add(new Product("Product3", 300.0f));
+		productRepository.add(new Product("Product4", 400.0f));
+		productRepository.add(new Product("Product5", 500.0f));
+		productRepository.add(new Product("Product6", 600.0f));
+		
+		sce.getServletContext().setAttribute("productRepository", productRepository);
+	}	
+	
+}
