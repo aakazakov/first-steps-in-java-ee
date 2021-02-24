5d446e0f77f09aa8ced3b7cc15ad783bc1a72e72
nito nise <aakazakovv@yandex.ru>
2021-02-24 14:22:47 +0300

    add prod repo

---
+++ hello-jsf/src/main/java/dev/fun/hello/repositories/ProductRepository.java
@@ -0,0 +1,62 @@
+package dev.fun.hello.repositories;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Named;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import dev.fun.hello.entities.Product;
+
+@Named
+@ApplicationScoped
+public class ProductRepository {
+
+	private static final Logger logger = LoggerFactory.getLogger(ProductRepository.class);
+
+	private final AtomicLong ID;
+
+	private final Map<Long, Product> repo;
+
+	public ProductRepository() {
+		repo = new HashMap<>();
+		ID = new AtomicLong(0L);
+	}
+	
+	@PostConstruct
+	public void fillWithData() {
+		add(new Product("Product1", 100));
+		add(new Product("Product2", 200));
+		add(new Product("Product3", 300));
+		add(new Product("Product4", 400));
+		add(new Product("Product5", 500));
+	}
+ 
+	public void add(Product p) {
+		if (p.getId() == null) {
+			p.setId(ID.incrementAndGet());
+		}
+		repo.put(p.getId(), p);
+		logger.info("[ADD] " + p);
+	}
+
+	public Product get(Long id) {
+		return repo.get(id);
+	}
+
+	public List<Product> getAll() {
+		return new ArrayList<>(repo.values());
+	}
+
+	public void delete(Long id) {
+		repo.remove(id);
+	}
+
+}
